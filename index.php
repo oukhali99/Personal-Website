bd2deebf7fdc0cd809aa30e9f39db3e3fec6e8cf
oukhali99 <oukhali@hotmail.com>
2020-02-18 13:31:47 -0500

    commit

--- index.php
+++ index.php
@@ -15,10 +15,8 @@
 			<p>
 				I'm a Math and Computer Science Major at McGill University. <br><br> Have you heard of TF2? Yeah well I wrote a BOT in python that scours three websites
 				looking for a low seller and a high buyer for what are called "unusual hats". Once my BOT found those two people, it acted as the middle man. 
-				Granted it made an average of five cents per transaction, but leaving it run overnight, and having ittrade in the Russian and Chinese market made 
-				for some nice passive income. And I would have made even more money but then I got banned for toO mAnY reQuEsts. Well sorry your servers are utter
-				garbage. <br><br> This served not only as programming experience, but as a life lesson as well. I learned that no matter where you go, there will
-				always be haters tryna keep a playa down.
+				Granted it made an average of five cents per transaction, but leaving it run overnight, and having it trade in the Russian and Chinese market made 
+				for some nice passive income. And I would have made even more money but then I got banned for too many requests. Sad.
 			</p>
 		</div>
 		<div class="container">
