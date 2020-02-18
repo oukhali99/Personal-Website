1d09f4c993297ea378a546b09d51837c8215d02a
Oussama Khalifeh <oussama.khalifeh@mail.mcgill.ca>
2020-02-18 13:21:21 -0500

    commit

--- index.php
+++ index.php
@@ -13,8 +13,12 @@
 				What do I do?
 			</h2>
 			<p>
-				I'll make you a simple website like this one for a good price. If you notice, as you rescale the browser,
-				the elements rearrange themselves and thus, this website is compatible with mobile devices.
+				I'm a Math and Computer Science Major at McGill University. <br><br> Have you heard of TF2? Yeah well I wrote a BOT in python that scours three websites
+				looking for a low seller and a high buyer for what are called "unusual hats". Once my BOT found those two people, it acted as the middle man. 
+				Granted it made an average of five cents per transaction, but leaving it run overnight, and having ittrade in the Russian and Chinese market made 
+				for some nice passive income. And I would have made even more money but then I got banned for toO mAnY reQuEsts. Well sorry your servers are utter
+				garbage. <br><br> This served not only as programming experience, but as a life lesson as well. I learned that no matter where you go, there will
+				always be haters tryna keep a playa down.
 			</p>
 		</div>
 		<div class="container">
