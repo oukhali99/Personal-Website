8ab5bac82c67b8ca760865c53869e412e02ce6c1
Oussama Khalifeh <oussama.khalifeh@mail.mcgill.ca>
2020-03-01 23:00:35 -0500

    commit

--- games.php
+++ games.php
@@ -9,6 +9,11 @@
 	</head>
 	<body>
 		<a href="https://play.google.com/store/apps/details?id=com.Samorg.Throw">Download Throw Game</a>
+		<br><img src="./res/images/1.png"/>
+		<br><img src="./res/images/2.png"/>
+		<br><img src="./res/images/3.png"/>
+		<br><img src="./res/images/4.png"/>
+
 		<?php
 			include "./res/php/footer.php";
 		?>
