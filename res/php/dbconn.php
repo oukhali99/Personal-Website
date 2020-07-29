0b02c51471e8b6703157c5a4a09c7a30d8ede0fd
Oussama <oukha@GamingPC>
2020-07-29 11:20:24 -0400

    commit

--- res/php/dbconn.php
+++ res/php/dbconn.php
@@ -2,7 +2,7 @@
 	define("DB_HOSTNAME", "valiant-soft.ca");
 	define("DB_USERNAME", "oussamak");
 	define("DB_PASSWORD", "password");
-	define("DB_DATABASE", "personal-website");
+	define("DB_DATABASE", "valiant_soft_ca");
 
 	$conn = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
