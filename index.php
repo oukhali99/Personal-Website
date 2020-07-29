35c4cf2685741bd3f0410944cf956e7bc6b21ba1
Oussama <oukha@GamingPC>
2020-07-29 11:38:40 -0400

    commit

--- index.php
+++ index.php
@@ -10,23 +10,16 @@
 		<img src="./res/images/palmtree.jpg" class="imageright">
 		<div class="containerleft">
 			<h2>
-				What do I do?
+				
 			</h2>
 			<p>
-				I'm a Math and Computer Science Major at McGill University. <br><br> Have you heard of TF2? Yeah well I wrote a BOT in python that scours three websites
-				looking for a low seller and a high buyer for what are called "unusual hats". Once my BOT found those two people, it acted as the middle man. 
-				Granted it made an average of five cents per transaction, but leaving it run overnight, and having it trade in the Russian and Chinese market made 
-				for some nice passive income. And I would have made even more money but then I got banned for too many requests. Sad.
+				
 			</p>
 		</div>
 		<div class="container">
 			<h2>
-				What do you need?
 			</h2>
 			<p>
-				I will make you a website, a Unity game, a python script, Java or C$ program and more. I have good experience in Web Crawling. If you'd like a
-				cheap website, I can make you one similar to this one and help you host it. My code is clean and it will be easy for you to bring it to another 
-				programmer for future modifications if I'm not around. Please email me with your requests and we can arrange a deal. oussama.khalifeh@mail.mcgill.ca
 			</p>
 		</div>
 		<?php
