22787bba6ced753626f85a5527d3c0c3e0ef7497
Oussama <oukha@GamingPC>
2020-07-30 10:56:32 -0400

    commit

--- logoff.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$pageName = "Logoff";
-	include_once "res/php/header.php";
-	include_once "res/php/functions.php";
-	include_once "res/php/dbconn.php";
-
-	session_destroy();
-	header("location: index.php");
-?>
