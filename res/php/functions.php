34fd131bd1e679f12dcd9625689199b000c599e8
Oussama <oukha@GamingPC>
2020-07-30 23:32:28 -0400

    commit

--- res/php/functions.php
+++ res/php/functions.php
@@ -1,16 +1,23 @@
 <?php
 	function get($conn, $name)
 	{
-		$raw = $_GET[$name];
-		return $raw;
-		//return mysqli_real_escape_string($conn, $raw);
+		return $_GET[$name];
+	}	
+
+	function display_error($error_string)
+	{
+		echo('<div style="color: white;">'.$error_string."</div>");
 	}
 
 	function email_exists($conn, $email)
-	{
-		$res = mysqli_query($conn, 'select * from Customers where email="'.$email.'";');
+	{		
+		$stmt = mysqli_stmt_init($conn);
+		mysqli_stmt_prepare($stmt, "SELECT * FROM Customers WHERE email=?");
+		mysqli_stmt_bind_param($stmt, "s", $email);
+		mysqli_stmt_execute($stmt);
+		$res = mysqli_stmt_get_result($stmt);
 		$count = mysqli_num_rows($res);
-		
+
 		return ($count != 0);
 	}
 
@@ -20,11 +27,6 @@
 		$match = preg_match($regexp, $email);
 
 		return ($match == 1);
-	}
-
-	function display_error($error_string)
-	{
-		echo('<div style="color: white;">'.$error_string."</div>");
 	}
 
 	function signup($email, $password, $display_name, $conn)
@@ -45,7 +47,7 @@
 				display_error("Mysql error: failed to initialize statement");
 			}
 
-			$succ = $stmt->prepare("INSERT INTO Customers (display_name, email, hashed_password) values(?,?,?);");
+			$succ = $stmt->prepare("INSERT INTO Customers (display_name, email, hashed_password, feedback_count) values(?,?,?,0);");
 			if (!$succ)
 			{
 				display_error("Mysql error: could not prepare mysql statement");
@@ -135,98 +137,134 @@
 		}
 	}
 
-	function feedback($email, $subject, $feedback, $maxFeedback, $conn)
+	function get_feedback_count($email, $conn)
 	{
-		// Get number of feedbacks
 		$stmt = $conn->stmt_init();
 		if (!$stmt)
 		{
-			display_error("Mysql error: failed to initialize statement");
+			return -1;
 		}
 
 		$succ = $stmt->prepare("SELECT feedback_count FROM Customers WHERE email=?");
 		if (!$succ)
 		{
-			display_error("Could not prepare mysql statement");		
+			return -1;
 		}
 		
 		$succ = $stmt->bind_param("s", $email);
 		if (!$succ)
 		{
+			return -1;
+		}
+
+		$succ = $stmt->execute();
+		if (!$succ)
+		{
+			return -1;
+		}
+		
+		$res = mysqli_stmt_get_result($stmt);
+		if (!$res)
+		{
+			return -1;
+		}
+
+		$mysql_entry = $res->fetch_assoc();
+		return $mysql_entry['feedback_count'];
+	}
+
+	function post_feedback($email, $subject, $feedback, $conn)
+	{
+		$stmt = $conn->stmt_init();
+		if (!$stmt)
+		{
+			return false;
+		}
+
+		$succ = $stmt->prepare("INSERT INTO Feedback(email, subject, feedback) VALUES(?, ?, ?)");
+		if (!$succ)
+		{
+			return false;	
+		}
+		
+		$succ = $stmt->bind_param("sss", $email, $subject, $feedback);
+		if (!$succ)
+		{
+			return false;
+		}
+
+		$succ = $stmt->execute();
+		if (!$succ)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	function set_feedback_count($email, $feedback_count, $conn)
+	{
+		$stmt = $conn->stmt_init();
+		if (!$stmt)
+		{
+			display_error("Mysql error: failed to initialize statement");
+			return false;
+		}
+
+		$succ = $stmt->prepare("UPDATE Customers SET feedback_count=? where email=?");
+		if (!$succ)
+		{
+			display_error("Could not prepare mysql statement");
+			return false;	
+		}
+		
+		$succ = $stmt->bind_param("ss", $feedback_count, $email);
+		if (!$succ)
+		{
 			display_error("MySql error: failed to bind param");
+			return false;
 		}
 
 		$succ = $stmt->execute();
 		if (!$succ)
 		{
 			display_error("Mysql error: failed to execute statement");
-		}
-		
-		$res = mysqli_stmt_get_result($stmt);
-		if (!$res)
-		{
-			display_error("Mysql error: failed to get result");
+			return false;
 		}
 
-		$mysql_entry = $res->fetch_assoc();
-		$feedback_count = $mysql_entry['feedback_count']; // THIS IS THE RESULT WE'RE LOOKING FOR
+		return true;
+	}
 
-		if (!$feedback_count || $feedback_count <= $maxFeedback)
+	function feedback($email, $subject, $feedback, $maxFeedback, $conn)
+	{
+		$feedback_count = get_feedback_count($email, $conn);
+
+		if ($feedback_count == -1)
 		{
-			// Post feedback
-			$stmt = $conn->stmt_init();
-			if (!$stmt)
-			{
-				display_error("Mysql error: failed to initialize statement");
-			}
-
-			$succ = $stmt->prepare("INSERT INTO Feedback(email, subject, feedback) VALUES(?, ?, ?)");
-			if (!$succ)
-			{
-				display_error("Could not prepare mysql statement");		
-			}
+			display_error("Failed to get feedback count");
+		}
+		elseif ($feedback_count < $maxFeedback)
+		{
+			$succ = post_feedback($email, $subject, $feedback, $conn);
 			
-			$succ = $stmt->bind_param("sss", $email, $subject, $feedback);
-			if (!$succ)
+			if ($succ)
 			{
-				display_error("MySql error: failed to bind param");
-			}
-
-			$succ = $stmt->execute();
-			if (!$succ)
-			{
-				display_error("Mysql error: failed to execute statement");
+				// Increment feedback
+				$feedback_count++;
+				$succ = set_feedback_count($email, $feedback_count, $conn);
+				if (!$succ)
+				{
+					display_error("Failed to increment feedback count");
+				}
 			}
 			else
 			{
-				// Increment feedback
-				$stmt = $conn->stmt_init();
-				if (!$stmt)
-				{
-					display_error("Mysql error: failed to initialize statement");
-				}
-
-				$succ = $stmt->prepare("UPDATE Customers SET feedback_count=? where email=?");
-				if (!$succ)
-				{
-					display_error("Could not prepare mysql statement");		
-				}
-				
-				$feedback_count++;
-				$succ = $stmt->bind_param("ss", $feedback_count, $email);
-				if (!$succ)
-				{
-					display_error("MySql error: failed to bind param");
-				}
-
-				$succ = $stmt->execute();
-				if (!$succ)
-				{
-					display_error("Mysql error: failed to execute statement");
-				}
-
-				header("location: contact.php");
+				display_error("Failed to post feedback");
 			}
+		}
+		else
+		{
+			display_error("Reached max feedback count of ".$maxFeedback);
 		}
 	}
 ?>
