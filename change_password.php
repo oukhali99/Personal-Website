7455dd057f04b445fa3ec398bc9a6fe84523f75f
Oussama <oukha@GamingPC>
2020-08-05 21:07:04 -0400

    Fixed change_password.php

--- change_password.php
+++ change_password.php
@@ -42,8 +42,8 @@
     <body>
         <div class="container">
             <h2>Reset Password</h2>
-            <form action="change_password.php" method="POST">
-                <input type="hidden" name="password_reset_token" value=<?php if (isset($_GET['password_reset_token'])) {echo '"'.$_GET['password_reset_token'].'"';} ?>><br>
+            <form action="change_password.php" method="GET">
+                <input type="hidden" name="password_reset_token" value=<?php if (isset($_POST['password_reset_token'])) {echo '"'.$_POST['password_reset_token'].'"';} elseif (isset($_GET['password_reset_token'])) {echo '"'.$_GET['password_reset_token'].'"';} ?>><br>
                 <div class="label">E-Mail</div>
                 <input name="email"><br><br>
                 <div class="label">Old Password</div>
